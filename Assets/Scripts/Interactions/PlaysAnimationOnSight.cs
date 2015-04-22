0233a7356381d24b1ea5535463a2fa7d8e4fbf5d
Dmitry Kurilchenko <dima.kurilchenko@gmail.com>
2015-04-22 14:56:56 +0500

    Improved script

---
+++ Assets/Scripts/Interactions/PlaysAnimationOnSight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaysAnimationOnSight : Interaction {
+
+	public bool isAbleToInteractAfterAnimation;
+	public GameObject receiver;
+
+	IEnumerator PlayAnimation() {
+		if (receiver == null) 
+			receiver = gameObject;
+		
+		var animation = receiver.GetComponent<Animation>();
+		animation.Play();
+
+		while(animation.isPlaying)
+			yield return null;
+
+		if (isAbleToInteractAfterAnimation) {
+			GetComponent<InteractiveThing>().isAbleToInteract = true;
+		}
+	}
+
+	public override void Interact () {
+		StartCoroutine(PlayAnimation());
+	}
+
+}
