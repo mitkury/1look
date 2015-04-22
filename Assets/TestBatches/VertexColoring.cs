2c38d4b36de3fc785ae590dc20436135a60ba7bf
Dmitry Kurilchenko <dima.kurilchenko@gmail.com>
2015-04-22 09:00:30 +0500

    Removed the point light at the center of the room, adjusted range of other point lights, changed recipe, changed the scene name

---
+++ Assets/TestBatches/VertexColoring.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VertexColoring : MonoBehaviour {
+
+	public Material targetMaterial;
+
+	void Start () {
+
+		var meshRenderer = GetComponent<MeshRenderer>();
+		var meshFilter = GetComponent<MeshFilter>();
+		var mesh = meshFilter.sharedMesh;
+		var colorsPerMaterial = GetColors(meshRenderer.sharedMaterials);
+		var newColors = new Color[mesh.vertexCount];
+
+		SetToSingleMaterial(meshRenderer);
+
+		for (int i = 0; i < mesh.subMeshCount; i++) {
+			var triangles = mesh.GetTriangles(i);
+
+			Debug.Log(triangles.Length);
+
+			for (int vi = 0; vi < triangles.Length; vi++) {
+				var vertex = mesh.vertices[triangles[vi]];
+			}
+
+			Debug.Log("---");
+		}
+
+	}
+
+	Color[] GetColors(Material[] materials) {
+		var colors = new Color[materials.Length];
+
+		for (int i = 0; i < materials.Length; i++) {
+			colors[i] = materials[i].color;
+		}
+
+		return colors;
+	}
+
+	void SetToSingleMaterial(MeshRenderer meshRenderer) {
+		var newMaterials = meshRenderer.sharedMaterials;
+
+		for (int i = 0; i < newMaterials.Length; i++) {
+			newMaterials[i] = targetMaterial;
+		}
+
+		meshRenderer.sharedMaterials = newMaterials;
+	}
+
+}
