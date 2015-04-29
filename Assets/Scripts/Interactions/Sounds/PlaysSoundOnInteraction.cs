b3b63e8fa285ca71385e39f287b21deba3a9fb30
Dmitry Kurilchenko <dima.kurilchenko@gmail.com>
2015-04-29 22:37:39 +0500

    Added poking sounds for bottles and a cloth sound for the hat

---
+++ Assets/Scripts/Interactions/Sounds/PlaysSoundOnInteraction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaysSoundOnInteraction : Interaction {
+
+	AudioSource _audioSource;
+
+	public AudioClip soundOnOpen;
+	public float lowPitchRange = 1f;
+	public float highPitchRange = 1f;
+
+	void Start() {
+		if (soundOnOpen != null) {
+			_audioSource = GetComponent<AudioSource>() != null ? GetComponent<AudioSource>() : gameObject.AddComponent<AudioSource>();
+			_audioSource.clip = soundOnOpen;
+		}	
+	}
+
+	public override void Interact ()
+	{
+		if (_audioSource != null) {
+			_audioSource.pitch = Random.Range(lowPitchRange, highPitchRange);
+			_audioSource.Play();
+		}
+	}
+}
