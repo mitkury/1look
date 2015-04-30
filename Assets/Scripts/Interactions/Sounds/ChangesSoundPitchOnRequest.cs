b99a8f366882b2d443d673937be84fea36c22293
Dmitry Kurilchenko <dima.kurilchenko@gmail.com>
2015-05-01 02:59:57 +0500

    Added fish splashes and a sound pitch changer to the painting

---
+++ Assets/Scripts/Interactions/Sounds/ChangesSoundPitchOnRequest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangesSoundPitchOnRequest : Interaction {
+
+	public AudioSource audioSourceReceiver;
+	public float max = 1f;
+	public float min = 0.5f;
+
+    void UpdateAudioValue(float value){
+		audioSourceReceiver.pitch = value;
+	}
+	
+	public void FadeIn(float time) {
+		LeanTween.cancel(gameObject);
+		LeanTween.value(gameObject, UpdateAudioValue, audioSourceReceiver.pitch, max, time);
+	}
+
+	public void FadeOut(float time) {
+		LeanTween.cancel(gameObject);
+		LeanTween.value(gameObject, UpdateAudioValue, audioSourceReceiver.pitch, min, time);
+	}
+}
---
+++ Assets/Scripts/Interactions/Sounds/PlaysSoundOnRequest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaysSoundOnRequest : SoundInteraction {
+
+	public AudioClipData[] sounds;
+
+	public void PlayOneShot (int index) {
+		var data = sounds[index];
+		base.PlayOneShot (data);
+	}
+
+}
