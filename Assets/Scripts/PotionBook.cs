03349d79b4d431c330f754c4b80be1ab131ddf20
Dmitry Kurilchenko <dima.kurilchenko@gmail.com>
2015-05-06 02:19:59 +0500

    Changed the book's behaviour

--- Assets/Scripts/PotionBook.cs
+++ Assets/Scripts/PotionBook.cs
@@ -3,11 +3,15 @@ using System.Collections;
 
 public class PotionBook : MonoBehaviour {
 
-	Visitor user;
+	Visitor visitor;
 	bool bookIsShown;
+	bool isInitiatedShowing;
+	bool isInitiatedHiding;
 
 	public float showBookAfterSec = 1f;
 	public float hideBookAfterSec = 2f;
+	public float showAnimationDuration = 2f;
+	public float hideAnimationDuration = 2f;
 	public Transform bookReadPoint;
 	public Transform bookHidePoint;
 
@@ -15,13 +19,14 @@ public class PotionBook : MonoBehaviour {
 		transform.position = bookHidePoint.position;
 		transform.rotation = bookHidePoint.rotation;
 
-		StartCoroutine(FindUserCo());
+		StartCoroutine(FindVisitorCo());
 	}
 
 	void Update() {
-		if (user == null)
+		if (visitor == null)
 			return;
 
+		/*
 		var targetPosition = bookHidePoint.position;
 		var targetRotation = bookHidePoint.rotation;
 		
@@ -32,13 +37,62 @@ public class PotionBook : MonoBehaviour {
 		
 		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 3);
 		transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 3);
+		*/
+
+		if (visitor.isLookingAtBookReadPosition) {
+			// Initiate showing the book if it's not yet shown.
+			if (!isInitiatedShowing && !bookIsShown) {
+				// If the book is in process of hiding—show it immediately.
+				if (isInitiatedHiding)
+					Show();
+				else
+					StartCoroutine(InitiateShowCo());
+			}
+		} 
+		else {
+			// Initiate hiding the book if it's shown.
+			if (!isInitiatedHiding && bookIsShown)
+				StartCoroutine(InitiateHideCo());
+		}
 	}
 
-	IEnumerator FindUserCo() {
+	IEnumerator InitiateShowCo() {
+		isInitiatedShowing = true;
+		yield return new WaitForSeconds(showBookAfterSec);
+		if (visitor.isLookingAtBookReadPosition) {
+			Show();
+		}
+		isInitiatedShowing = false;
+	}
+
+	IEnumerator InitiateHideCo() {
+		isInitiatedHiding = true;
+		yield return new WaitForSeconds(hideBookAfterSec);
+		if (!visitor.isLookingAtBookReadPosition) {
+			Hide();
+		}
+		isInitiatedHiding = false;
+	}
+
+	void Show() {
+		bookIsShown = true;
+		LeanTween.cancel(gameObject);
+		LeanTween.move(gameObject, bookReadPoint.position, showAnimationDuration).setEase(LeanTweenType.easeInOutCubic);
+		LeanTween.rotate(gameObject, bookReadPoint.rotation.eulerAngles, showAnimationDuration).setEase(LeanTweenType.easeInOutCubic);
+	}
+
+	void Hide() {
+		bookIsShown = false;
+		LeanTween.cancel(gameObject);
+		LeanTween.move(gameObject, bookHidePoint.position, hideAnimationDuration).setEase(LeanTweenType.easeInOutCubic);
+		LeanTween.rotate(gameObject, bookHidePoint.rotation.eulerAngles, hideAnimationDuration).setEase(LeanTweenType.easeInOutCubic);
+	}
+
+	IEnumerator FindVisitorCo() {
 		if (King.visitor == null)
 			yield return null;
 
-		user = King.visitor;
+		visitor = King.visitor;
 	}
 
 }
