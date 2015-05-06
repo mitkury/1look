3c20b192363b97a0775790f9a774dd7381919006
korigame <Miles@Miless-iMac.local>
2015-05-06 14:44:32 -0700

    Initial credits test rendered to texture on TV

---
+++ Assets/ScrollUp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollUp : MonoBehaviour {
+
+	public float rate = 1.0f;
+
+	void Update () 
+	{
+		Vector3 pos = transform.position;
+		pos.y += rate * Time.deltaTime;
+		transform.position = pos;
+	}
+}
