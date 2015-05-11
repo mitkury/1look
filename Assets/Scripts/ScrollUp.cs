34538bc4c44bd31f0877e1c445cf7879a0ec9f31
korigame <Miles@Miless-iMac.local>
2015-05-11 07:10:57 -0700

    Removed old popcorn sound files. Updated and cross checked credits.txt and applied new font to credits scroll object. Made credits text loop on TV

--- Assets/Scripts/ScrollUp.cs
+++ Assets/Scripts/ScrollUp.cs
@@ -4,11 +4,24 @@ using System.Collections;
 public class ScrollUp : MonoBehaviour {
 
 	public float rate = 1.0f;
+	private float defaultY;
+	public float loopY;
+
+	void Start ()
+	{
+		defaultY = transform.position.y;
+	}
 
 	void Update () 
 	{
 		Vector3 pos = transform.position;
 		pos.y += rate * Time.deltaTime;
+
+		// Move credits back to starting position so they can loop
+		if(pos.y > loopY)
+			pos.y = defaultY;
+
 		transform.position = pos;
+
 	}
 }
